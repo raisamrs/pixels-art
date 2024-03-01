8516603cbda6b78dda0e968d736b66281cd6f4ed
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2024-03-01 14:11:36 -0300

    chore: mudanças na geração de cores

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const arrPixelsBoard = document.getElementsByClassName('pixel');
 const pixelBoardContainer = document.getElementById('pixel-board-container');
 const selectedColorBlock = document.getElementById('selected-color');
 const divBtnRandomColors = document.getElementById('btn-random-color-container');
+const btnRandomColorsId = ('button-random-color');
 
 /* 2 - Adicione à página um quadro contendo 25 pixels, sendo que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel */
 
@@ -22,14 +23,16 @@ const color3 = document.getElementById('color-3');
 const color4 = document.getElementById('color-4');
 
 let selectedColor = null;
+let colors = [];
 
 const clickedColor = (event) => {
-  if (selectedColor) {
+  if (selectedColor && selectedColor.id !== 'selected-color') {
     selectedColor.classList.remove('selected');
   }
   event.target.classList.add('selected');
   selectedColor = event.target;
-  const clickedColorBlock = getComputedStyle(event.target).backgroundColor;
+  const colorIndex = Number(event.target.id.split('-')[1]) - 1;
+  const clickedColorBlock = colors[colorIndex];
   selectedColorBlock.style.backgroundColor = clickedColorBlock;
 };
 
@@ -67,36 +70,50 @@ clearBoardBtn.addEventListener('click', () => {
 
 /* 6 - Adicione um botão para gerar cores aleatórias para a paleta de cores */
 const changeColorsBtn = document.createElement('button');
-changeColorsBtn.id = 'button-random-color';
+changeColorsBtn.id = btnRandomColorsId;
 changeColorsBtn.className = 'btns';
 changeColorsBtn.innerText = 'Gerar cores aleatórias';
 divBtnRandomColors.appendChild(changeColorsBtn);
 
-function generateColorRgb(usedColors) {
-  let color;
-  do {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    color = `rgb(${r}, ${g}, ${b})`;
-  } while (usedColors.has(color));
-
-  usedColors.add(color);
-  return color;
+function generateColorRgb() {
+  const usedColors = new Set();
+  colors = [];
+  for (let i = 0; i < 4; i += 1) {
+    let color;
+    do {
+      const r = Math.floor(Math.random() * 255);
+      const g = Math.floor(Math.random() * 255);
+      const b = Math.floor(Math.random() * 255);
+      color = `rgb(${r}, ${g}, ${b})`;
+    } while (usedColors.has(color));
+    usedColors.add(color);
+    colors.push(color);
+  }
 }
 
-document.getElementById('button-random-color').addEventListener('click', () => {
-  const usedColors = new Set();
+document.getElementById(btnRandomColorsId).addEventListener('click', generateColorRgb);
+
+function updateColorElements() {
   const colorElements = document.querySelectorAll('.color');
-
   colorElements.forEach((colorElement, i) => {
-    const newColor = generateColorRgb(usedColors);
-    const newColorElement = colorElement;
-    newColorElement.style.backgroundColor = newColor;
-    newColorElement.style.setProperty('--color-identifier', i);
+    if (colorElement !== selectedColorBlock) {
+      const newColor = colors[i];
+      const newColorElement = colorElement;
+      newColorElement.style.backgroundColor = newColor;
+      newColorElement.style.setProperty('--color-identifier', i);
+    }
   });
+  color1.classList.remove('selected');
+  color2.classList.remove('selected');
+  color3.classList.remove('selected');
+  color4.classList.remove('selected');
+  selectedColorBlock.classList.add('selected');
+  selectedColor = selectedColorBlock;
+}
+document.getElementById(btnRandomColorsId).addEventListener('click', () => {
+  generateColorRgb();
+  updateColorElements();
 });
-
 /* 7 - Crie uma função para salvar e recuperar o seu desenho atual no localStorage
 Ao recarregar a página, o quadro deve permanecer. Para isso, a cada clique em um pixel, salve a cor e a posição no localStorage. Ao recarregar a página o quadro deverá ser recuperado a partir dos dados que foram salvos no localStorage. */
 function saveDraw() {
